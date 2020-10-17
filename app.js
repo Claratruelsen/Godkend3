d8260090e424c11f1e41f48e347dad2494067b89
Claratruelsen <clara.l.truelsen@gmail.com>
2020-10-17 13:55:16 +0200

    tilføjet alle CRUD endpoints

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app=express();
 const port= process.env.port || 3001;
 
 
-// mine GET requests: 
+// mine GET requests CRUD: R - read : 
 const users = require("./controller/userController.js");
 app.use("/users", users)
 
@@ -13,6 +13,19 @@ app.use("/interests", interest)
 
 const match= require("./controller/matchController.js");
 app.use("/matches", match)
+
+// andre CRUD endpoints - dog bare med beskeder frem for virkelig at kunne noget
+app.delete("/deleteUser", (req, res)=>{
+    res.json({message: "You have deleted a user"})
+});
+//crud : U - update= put
+app.put("/updateInterest", (req, res)=>{
+    res.json({message:"You have updates your interests" })
+});
+// crud: C - create
+app.post("/createNewUser", (req, res)=>{
+    res.json({message: "You have created a new user"})
+}); 
 
 
 //server aktiveres
